c6c95fe4e698aa4547d07c7c98ee60aea8bcda5a
Durim Aliu <DurimAliu@Durims-iMac.local>
2015-06-06 17:34:56 +0200

    Initial commit

---
+++ app/src/main/java/com/aliudurim/myappportfolio/MainActivity.java
@@ -0,0 +1,65 @@
+package com.aliudurim.myappportfolio;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private Toast toast;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+
+        cancelToast();
+
+        Button button = (Button) view;
+        String textButton = (String) button.getText();
+        Context context = getApplicationContext();
+
+        CharSequence text = getString(R.string.textLaunch) + " " + textButton.toLowerCase();
+
+        toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void cancelToast() {
+
+        if (toast != null) {
+            toast.cancel();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
